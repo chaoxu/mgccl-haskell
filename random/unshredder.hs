bc15ba5b77bace8426e4f287f776db325c99313c
Mgccl <mgcclx@gmail.com>
2011-11-13 19:19:45 +0000

    updated more

--- random/unshredder.hs
+++ random/unshredder.hs
@@ -4,71 +4,56 @@ import Data.Array.Unboxed
 import Control.Applicative
 import Data.Ord
 
+--config the metric
+metric = (sqrt.sqrt)
+
 main = do
   ilInit
   t <- readImage "file.png"
-  --config the metric
-  let metric = (sqrt.sqrt)
   let img = t
-  let stripWidth = stripFind img metric
+  let stripWidth = stripFind img
   let list = (decode img stripWidth)
-  let result = concat (hash (unshred list metric) list)
-  writeImage "unshredded.png" (encode result)
+  let result = concat (hash (unshred list) list)
+  writeImage "unshredded.png" (encode (snd $ bounds img) result)
 
-hash [] _      = []
 hash x y       = foldr ((:).(y!!)) [] x
+width img      = (w+1) where (_,w,_) = snd $ bounds img
+height img     = (h+1) where (h,_,_) = snd $ bounds img
+elemIndex' a b = map (head . (flip elemIndices) b) a
 
-width img      = (w+1) where (_,w,_) = snd (bounds img)
-height img     = (h+1) where (h,_,_) = snd (bounds img)
-elemIndex' a b = (map (head . (flip elemIndices) b) a)
-
---parse the image into lists for easier manipulation
-decode img stripWidth = toStrips' 0
-  where 
-  toStrips' start | start == (width img)  = []
-                  | otherwise             = (toStrip2D start (start+stripWidth)):(toStrips' (start+stripWidth))
-
-  toStrip2D x xend | x == xend   = []
-                   | otherwise   = (toStrip1D x 0):(toStrip2D (x+1) xend)
-
-  toStrip1D x y | y == (height img)  = []
-                | otherwise          = ((color 0):(color 1):(color 2):[color 3]):(toStrip1D x (y+1))
-                where color k  = (img!(y,x,k))
+--convert the img into lists
+decode img stripWidth = chunk stripWidth [[[img!(y,x,z)|z<-[0..3]]|y<-[0..((height img)-1)]]|x<-[0..((width img)-1)]]
+                      where chunk n = takeWhile (not.null) . map (take n) . iterate (drop n)
 
 --convert the lists back to the image
-encode :: [[[Word8]]] -> UArray (Int, Int, Int) Word8
-encode list = array ((0,0,0),(ymax,xmax,3)) arr
-             where arr = zip [(y,x,z)|(x,y,z)<-range((0,0,0),(xmax,ymax,3))] [x|z<-list,y<-z,x<-y]
-                   xmax = (length list)-1
-                   ymax = (length (head list))-1 
+encode :: (Int,Int,Int) -> [[[Word8]]] ->UArray (Int, Int, Int) Word8
+encode bound list = array ((0,0,0),bound) (zip [(y,x,z)|(y,x,z)<-range((0,0,0),bound)] [y|z<-(transpose list),x<-z,y<-x])
 
 --function calculates the difference between two columns
-diff xs ys metric = sum (map (uncurry deviation) (zip xs ys))
-           where  deviation a b = distance (map fromIntegral a) (map fromIntegral b)
-                  distance a b = (metric . sum) (map (\x -> (abs (fst x - snd x))) (zip a b))
+diff xs ys = sum $ map (uncurry distance) (zip (last xs) (head ys))
+           where distance a b = metric $ sum $ map (abs . (uncurry (-))) (zip (map fromIntegral a) (map fromIntegral b))
 
 --find best strip width
-stripFind img metric = foldr1 gcd [divisors!!x |x<-top5]
+stripFind img = foldr1 gcd [divisors!!x |x<-top5]
               where list = map stripFind' divisors
                     top5 = take 5 (elemIndex' ((reverse.sort) list) list)
                     divisors = [n| n<-[2..(div (width img) 2)], mod (width img) n == 0]
-                    stripFind' n = avg [diff (last (strips!!i)) (head (strips!!(i+1))) metric | i<-[0..((length strips)-2)]]
-                         where strips  = (decode img n)
+                    stripFind' n = avg (map (uncurry diff) (zip (init strips) (tail strips)))
+                         where strips  = decode img n
                                avg xs = (sum xs) / (fromIntegral $ length xs)
 --unshred!
---unshred strips metric = unshredFrom ((length strips)-1)
-unshred strips metric = minimumBy (comparing weight) [n:search bound n [n] | n<-[0..bound]]
+unshred strips = minimumBy (comparing weight) [n:search bound n [n] | n<-[0..bound]]
   where
     --weight of each edge
-    edgeWeight =  [ [(\a b -> diff (last a) (head b) metric) y x | x <- strips] | y <- strips]
+    edgeWeight =  [ [diff y x | x <- strips] | y <- strips]
     
     --sort the edges on each vertex by weight
     sortedEdges = map (uncurry elemIndex') (zip (map sort edgeWeight) edgeWeight)
     
     --search for the path
     search 0 _ _ = []
-    search items n chosen = x:(search (items-1) x (x:chosen))
-                          where x = head (dropWhile ((flip elem) chosen) (sortedEdges!!n))
+    search items n chosen = x:search (items-1) x (x:chosen)
+                          where x = head $ dropWhile (flip elem $ chosen) (sortedEdges!!n)
     
     --Find weight of a certain path
     weight = maximum . map (uncurry (\x y-> edgeWeight!!x!!y)) . liftA2 zip init tail

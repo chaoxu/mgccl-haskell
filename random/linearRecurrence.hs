66f3b9db240d98c68e4e3a799727ba7c951cff57
Mgccl <mgcclx@gmail.com>
2011-11-30 05:52:05 +0000

    Made it faster

--- random/linearRecurrence.hs
+++ random/linearRecurrence.hs
@@ -1,17 +1,7 @@
 import Data.List
 
 --Lazy linear homogeneous recurrence relations with constant coefficient
-linearRecurrence coef term = base ++ map (sum . (zipWith (*) coef)) (map (take n) (tails a))
-  where a = linearRecurrence coef term
-        base = replicate (n - (length bterm)) 0 ++ bterm
-        bterm = (take n term)
-        n = (length coef)
-
---Generalize the above to linear homogeneous recurrence relation in any field
---Note the zero for * should be the zero for +.
-generalLinearRecurrence coef term mulOperator plusOperator zero = 
-  base ++ map ((foldr plusOperator zero) . (zipWith mulOperator coef)) (map (take n) (tails a))
-  where a = generalLinearRecurrence coef term mulOperator plusOperator zero
-        base = replicate (n - (length bterm)) zero ++ bterm
-        bterm = (take n term)
+linearRecurrence coef base | n /= (length base) = []
+                           | otherwise = a
+  where a = base ++ map (sum . (zipWith (*) coef)) (map (take n) (tails a))
         n = (length coef)

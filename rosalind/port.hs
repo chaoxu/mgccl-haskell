b737d89cbdd284821de3def447ff17a68ad3559e
Chao Xu <mgcclx@gmail.com>
2012-11-27 02:33:16 -0500

    update

--- rosalind/port.hs
+++ rosalind/port.hs
@@ -3,5 +3,5 @@ import Data.List
 import Data.Maybe
 main :: IO ()
 main  = do c <- getLine
-           print $ takeWhile (/='X') $ foldr (\x y->(fromJust $ lookup x t):y) "" $ chunksOf 3 c
+           print $ takeWhile (/='X') $ map (fromJust . flip lookup t) $ chunksOf 3 c
 t = map (\[a,b,c,d]->([a,b,c],d)) $ chunksOf 4 "UUUFCUULAUUIGUUVUUCFCUCLAUCIGUCVUUALCUALAUAIGUAVUUGLCUGLAUGMGUGVUCUSCCUPACUTGCUAUCCSCCCPACCTGCCAUCASCCAPACATGCAAUCGSCCGPACGTGCGAUAUYCAUHAAUNGAUDUACYCACHAACNGACDCAAQAAAKGAAECAGQAAGKGAGEUGUCCGURAGUSGGUGUGCCCGCRAGCSGGCGCGARAGARGGAGUGGWCGGRAGGRGGGGUGAXUAGXUAAX"
